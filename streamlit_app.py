5e78b1d25c0c8416740bac4b27e3b7b5845b2a58
Srujan-ay <srujanay43@gmail.com>
2024-08-28 16:13:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,8 +21,8 @@ st.write('the name of youe smoothie will be ', name_on_order)
 
 session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('search_on'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
-#st.stop()
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 pd_df=my_dataframe.to_pandas()
 #st.dataframe(pd_df)
@@ -41,8 +41,8 @@ if ingredients_list:
    for fruit_chosen in ingredients_list:
        ingredients_string += fruit_chosen+ ' '
        
-       search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-       st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+       #search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+       #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
 
     
    #st.write(ingredients_string)
